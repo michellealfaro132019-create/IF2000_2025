2a1315e060c43b55038ff691acc802e892da5e47
Ashly <michellealfaro132019@gmail.com>
2025-08-18 22:50:15 -0600

    Mi primer commit en java

---
+++ IF2000_introduccionprogra/IF2000_introducionprogra.java
@@ -0,0 +1,28 @@
+package IF2000_introduccionprogra;
+
+public class IF2000_introducionprogra {
+    public static void main(String[] args) {
+        
+        String name = "Lionel"; 
+        int age = 40;
+     
+        System.out.println("Hola mundo"); 
+        //Python: println('Hola mundo')
+        System.out.println("Name:"+ name); 
+        System.out.println("Age:" +age); 
+
+        /*Tipos de datos numericos: int, long, double, short, float,numeric, money, decimal
+         * 
+         * Manejo de fechas: date
+         * 
+         * Cadenas de caracteres: String, char
+         * 
+         * Objetos: object
+         * 
+         * Data estructura: Arrays or Vectors: int [] x; 
+         *                                     int [][] y; 
+         * True or false
+         * boolean 
+         */
+    }
+}
---
+++ domain/Person.java
@@ -0,0 +1,57 @@
+package domain;
+
+public class Person{
+
+//Atributes global 
+private String name; 
+private String lastName; 
+private String id; 
+private String phone; 
+private int age; 
+ 
+//Constructor
+public Person(String name, String lastName,String id, String phone,int age ){
+this.name=name; 
+this.lastName=lastName; 
+this.id=id; 
+this.phone=phone; 
+this.age=age; 
+}
+
+public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastName;
+    }
+    public void setLastname(String lastname) {
+        lastName = lastname;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public int getAge() {
+        return age;
+    }   
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Person [name=" 
+        + getName() + ", Lastname=" + getLastname()+ ", id=" + getId()+ ", phone=" + getPhone() + ", age=" + getAge()
+                + "]";
+    }
+}
