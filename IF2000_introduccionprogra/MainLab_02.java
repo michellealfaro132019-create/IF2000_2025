66212caf0a8398973a84207073034b17021c2498
michellealfaro132019-create <michellealfaro132019@gmail.com>
2025-09-04 21:40:20 -0600

    laboratorio dos

---
+++ IF2000_introduccionprogra/MainLab_02.java
@@ -0,0 +1,12 @@
+package IF2000_introduccionprogra;
+import logic.*;
+public class MainLab_02{
+    public static void main(String args[]){
+
+        lab_2 lab =new lab_2(); 
+        lab.SumaPares(); 
+        lab.CelsiusAFahrenheit();
+    }
+}
+    
+
